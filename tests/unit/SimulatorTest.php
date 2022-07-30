6f3c1fcc744b4b152b3fcf07f38bf2667cd19c74
Hiep Tran <63086258+hieptran87@users.noreply.github.com>
2022-07-30 10:19:40 +0700

    Update unit test

--- tests/unit/SimulatorTest.php
+++ tests/unit/SimulatorTest.php
@@ -1,22 +1,6 @@
 <?php
 
 /**
- * O2Web\HockeyPlayoffs\RowTraitTest
- *
- * PHP version 7
- *
- * @author    Hiep Tran <tranquyhiep87@gmail.com>
- * @copyright 2022 O2Web <info@o2web.com>
- * @license   https://opensource.org/licenses/MIT
- * @link      https://github.com/hieptran87/hockey-playoffs
- * @link      http://o2web.ca
- */
-
-namespace O2Web\HockeyPlayoffs;
-use O2Web\HockeyPlayoffs\Entities\Team;
-use PHPUnit\Framework\TestCase;
-
-/**
  * Test class for the Simulator implementation.
  *
  * @author    Hiep Tran <tranquyhiep87@gmail.com>
@@ -25,11 +9,15 @@ use PHPUnit\Framework\TestCase;
  * @link      https://github.com/hieptran87/hockey-playoffs
  * @link      http://o2web.ca
  */
+namespace O2Web\HockeyPlayoffs;
+use O2Web\HockeyPlayoffs\Entities\Team;
+use PHPUnit\Framework\TestCase;
+
 class SimulatorTest extends TestCase
 {
 
     /**
-     * The exportable trait that has to be tested.
+     * The exportable Simulator that has to be tested.
      *
      * @var \O2Web\HockeyPlayoffs\Simulator
      */
@@ -54,8 +42,6 @@ class SimulatorTest extends TestCase
      */
     public function testSimulate()
     {
-         // query whether or not the given value is available and a number
-         $value = 1;
          $this->assertContainsOnlyInstancesOf(Team::class, [$this->simulator->simulate()]);
     }
 }
